cdb55e2acc7740606f926755237d19c37ed369fd
Ryan Parr <parrfolio@gmail.com>
2021-12-12 11:51:59 -0800

    root adjustments

--- src/server/index.js
+++ src/server/index.js
@@ -12,7 +12,7 @@ app.use(express.static(webroot));
 //for routing
 app.get("*", function(req, res) {
   res.sendFile("index.html", {
-    root: path.join(__dirname, "/"),
+    root: webroot,
   });
 });
 
