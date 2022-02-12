abe185abc5334f512ceb37857addc25a8238665b
Ryan Parr <parrfolio@gmail.com>
2022-02-12 13:43:35 -0800

    test

--- src/server/index.js
+++ src/server/index.js
@@ -67,57 +67,57 @@ const sleep = (milliseconds) => {
 
 //pulse train 1
 io.sockets.on("connection", function(socket) {
-  // socket.on("direction", function(data) {
-  //   console.log("DATA: ", data);
-  //   console.log("Selection", data.selection);
-  //   console.log("===-- SELECTION --===", data.selection);
-  //   console.log(
-  //     "===-- Pulse Train Rel --===",
-  //     data.ptrains[0],
-  //     data.ptrains[1]
-  //   );
+  socket.on("direction", function(data) {
+    console.log("DATA: ", data);
+    console.log("Selection", data.selection);
+    console.log("===-- SELECTION --===", data.selection);
+    console.log(
+      "===-- Pulse Train Rel --===",
+      data.ptrains[0],
+      data.ptrains[1]
+    );
 
-  //   if (data.state === "on") {
-  //     (async function() {
-  //       console.log("=======-- Train 1 START --=======");
-  //       for (let i = 0; i < data.ptrains[0]; i++) {
-  //         await sleep(pulseSpeed);
-  //         gpio.write(pin, true, function(err) {
-  //           console.log("on");
-  //           if (err) throw err;
-  //           (async function() {
-  //             await sleep(pulseDelay);
-  //             gpio.write(pin, false);
-  //             console.log("off");
-  //           })();
-  //         });
-  //       }
-  //     })();
+    if (data.state === "on") {
+      (async function() {
+        console.log("=======-- Train 1 START --=======");
+        for (let i = 0; i < data.ptrains[0]; i++) {
+          await sleep(pulseSpeed);
+          gpio.write(pin, true, function(err) {
+            console.log("on");
+            if (err) throw err;
+            (async function() {
+              await sleep(pulseDelay);
+              gpio.write(pin, false);
+              console.log("off");
+            })();
+          });
+        }
+      })();
 
-  //     // pulse train 2
-  //     (async function() {
-  //       await sleep(data.ptrainDelay);
-  //       console.log("=======-- Train 2 START --=======");
-  //       for (let i = 0; i < data.ptrains[1]; i++) {
-  //         await sleep(pulseSpeed);
-  //         gpio.write(pin, true, function(err) {
-  //           console.log("on");
-  //           if (err) throw err;
-  //           (async function() {
-  //             await sleep(pulseDelay);
-  //             gpio.write(pin, false);
-  //             console.log("off");
-  //           })();
-  //         });
-  //       }
-  //     })();
-  //   } else if (data.state === "off") {
-  //     gpio.write(pin, false);
-  //   } else {
-  //     // By default we turn off the motors
-  //     gpio.write(pin, false);
-  //   }
-  // });
+      // pulse train 2
+      (async function() {
+        await sleep(data.ptrainDelay);
+        console.log("=======-- Train 2 START --=======");
+        for (let i = 0; i < data.ptrains[1]; i++) {
+          await sleep(pulseSpeed);
+          gpio.write(pin, true, function(err) {
+            console.log("on");
+            if (err) throw err;
+            (async function() {
+              await sleep(pulseDelay);
+              gpio.write(pin, false);
+              console.log("off");
+            })();
+          });
+        }
+      })();
+    } else if (data.state === "off") {
+      gpio.write(pin, false);
+    } else {
+      // By default we turn off the motors
+      gpio.write(pin, false);
+    }
+  });
 
   socket.on("lights", function(data) {
     console.log("Lights State", data.state);
