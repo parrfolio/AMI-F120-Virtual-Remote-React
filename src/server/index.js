597d62414ff4ef3b5f7223496a78ca4957ef0992
Ryan Parr <parrfolio@gmail.com>
2022-02-15 12:56:16 -0800

    test

--- src/server/index.js
+++ src/server/index.js
@@ -107,51 +107,14 @@ io.sockets.on("connection", function(socket) {
   socket.on("lights", function(data) {
     console.log("Lights State", data.state);
     if (data.state === "on") {
-      var NUM_LEDS = parseInt(process.argv[2], 10) || 10,
-        pixelData = new Uint32Array(NUM_LEDS);
+      const channel = ws281x(100, { stripType: "ws2812" });
 
-      ws281x.init(NUM_LEDS);
-      console.log(ws281x.init(NUM_LEDS));
-
-      // ---- trap the SIGINT and reset before exit
-      process.on("SIGINT", function() {
-        ws281x.reset();
-        process.nextTick(function() {
-          process.exit(0);
-        });
-      });
-
-      // ---- animation-loop
-      var offset = 0;
-      setInterval(function() {
-        for (var i = 0; i < NUM_LEDS; i++) {
-          //console.log(pixelData[i]);
-          pixelData[i] = colorwheel((offset + i) % 256);
-        }
-
-        offset = (offset + 1) % 256;
-        ws281x.render(pixelData);
-      }, 1000 / 30);
-
-      console.log("Press <ctrl>+C to exit.");
-
-      // rainbow-colors, taken from http://goo.gl/Cs3H0v
-      function colorwheel(pos) {
-        pos = 255 - pos;
-        if (pos < 85) {
-          return rgb2Int(255 - pos * 3, 0, pos * 3);
-        } else if (pos < 170) {
-          pos -= 85;
-          return rgb2Int(0, pos * 3, 255 - pos * 3);
-        } else {
-          pos -= 170;
-          return rgb2Int(pos * 3, 255 - pos * 3, 0);
-        }
+      const colorArray = channel.array;
+      for (let i = 0; i < channel.count; i++) {
+        colorsArray[i] = 0xffcc22;
       }
 
-      function rgb2Int(r, g, b) {
-        return ((r & 0xff) << 16) + ((g & 0xff) << 8) + (b & 0xff);
-      }
+      ws281x.render();
     }
   });
 });
