de8f1b8519905c26756fc425e6526c95704876da
Ryan Parr <parrfolio@gmail.com>
2022-02-15 16:41:57 -0800

    it works

--- src/server/index.js
+++ src/server/index.js
@@ -43,8 +43,22 @@ const pulseDelay = 30;
 gpio.setup(pin, gpio.DIR_OUT);
 
 //lights!
-// const lightpin = 18;
-// gpio.setup(lightpin, gpio.DIR_OUT);
+let colorwheel = (pos) => {
+  pos = 255 - pos;
+  if (pos < 85) {
+    return rgb2Int(255 - pos * 3, 0, pos * 3);
+  } else if (pos < 170) {
+    pos -= 85;
+    return rgb2Int(0, pos * 3, 255 - pos * 3);
+  } else {
+    pos -= 170;
+    return rgb2Int(pos * 3, 255 - pos * 3, 0);
+  }
+};
+
+let rgb2Int = (r, g, b) => {
+  return ((r & 0xff) << 16) + ((g & 0xff) << 8) + (b & 0xff);
+};
 
 const sleep = (milliseconds) => {
   return new Promise((resolve) => setTimeout(resolve, milliseconds));
@@ -110,11 +124,18 @@ io.sockets.on("connection", function(socket) {
       const channel = ws281x(100, { stripType: "ws2812" });
 
       const colorsArray = channel.array;
-      for (let i = 0; i < channel.count; i++) {
-        colorsArray[i] = 0xcc9900;
-      }
 
-      ws281x.render();
+      ws281x.init(channel.count);
+
+      var offset = 0;
+      setInterval(function() {
+        for (var i = 0; i < channel.count; i++) {
+          pixelData[i] = colorwheel((offset + i) % 256);
+        }
+
+        offset = (offset + 1) % 256;
+        ws281x.render(pixelData);
+      }, 1000 / 30);
     }
   });
 });
