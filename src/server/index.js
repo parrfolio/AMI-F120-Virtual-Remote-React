06d19a33bd06b9c33d5bef492a0b0e6ac0ddcda2
Ryan Parr <parrfolio@gmail.com>
2022-02-16 09:54:11 -0800

    testing color patterns

--- src/server/index.js
+++ src/server/index.js
@@ -111,12 +111,30 @@ io.sockets.on("connection", function(socket) {
 
       const colorsArray = channel.array;
       for (let i = 0; i < channel.count; i++) {
-        colorsArray[i] = 0xffcc22;
+        //colorsArray[i] = 0xffcc22;
+        colorsArray[i] = colorwheel((offset + i) % 256);
       }
 
-      ws281x.render();
+      ws281x.render(colorsArray);
 
-      console.log(channel.count, pixelData);
+      function colorwheel(pos) {
+        pos = 255 - pos;
+        if (pos < 85) {
+          return rgb2Int(255 - pos * 3, 0, pos * 3);
+        } else if (pos < 170) {
+          pos -= 85;
+          return rgb2Int(0, pos * 3, 255 - pos * 3);
+        } else {
+          pos -= 170;
+          return rgb2Int(pos * 3, 255 - pos * 3, 0);
+        }
+      }
+
+      function rgb2Int(r, g, b) {
+        return ((r & 0xff) << 16) + ((g & 0xff) << 8) + (b & 0xff);
+      }
+
+      console.log(channel.count);
     }
   });
 });
