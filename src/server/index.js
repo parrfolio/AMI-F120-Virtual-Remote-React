a580453fd4d613a2b078995ca86a525db517c999
Ryan Parr <parrfolio@gmail.com>
2022-02-15 16:54:14 -0800

    test

--- src/server/index.js
+++ src/server/index.js
@@ -109,7 +109,27 @@ io.sockets.on("connection", function(socket) {
     if (data.state === "on") {
       var NUM_LEDS = parseInt(process.argv[2], 10) || 10,
         pixelData = new Uint32Array(NUM_LEDS);
+      ws281x.init(NUM_LEDS);
+      process.on("SIGINT", function() {
+        ws281x.reset();
+        process.nextTick(function() {
+          process.exit(0);
+        });
+      });
+      // ---- animation-loop
+      var offset = 0;
+      setInterval(function() {
+        var i = NUM_LEDS;
+        while (i--) {
+          pixelData[i] = 0;
+        }
+        pixelData[offset] = 0xffffff;
 
+        offset = (offset + 1) % NUM_LEDS;
+        ws281x.render(pixelData);
+      }, 100);
+
+      console.log("Press <ctrl>+C to exit.");
       console.log(NUM_LEDS, pixelData);
     }
   });
