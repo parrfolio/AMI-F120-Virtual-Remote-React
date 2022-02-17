38aad9b93e0b9e4f7cd9e45047e0a50457512221
Ryan Parr <parrfolio@gmail.com>
2022-02-17 12:55:03 -0800

    test

--- src/server/index.js
+++ src/server/index.js
@@ -172,7 +172,7 @@ io.sockets.on("connection", function(socket) {
     } else {
       console.log("Shut down", data.state);
       clearInterval(rainbowInterval);
-      // ws281x.reset(colorsArray);
+      ws281x.reset(colorsArray);
       ws281x.finalize(colorsArray);
     }
   });
