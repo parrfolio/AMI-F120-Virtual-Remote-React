20ca8c182755a8b4c4647ecfcebdcf234a9c3b3d
Ryan Parr <parrfolio@gmail.com>
2022-02-16 16:13:43 -0800

    test

--- src/server/index.js
+++ src/server/index.js
@@ -50,23 +50,6 @@ const sleep = (milliseconds) => {
   return new Promise((resolve) => setTimeout(resolve, milliseconds));
 };
 
-const colorwheel = (pos) => {
-  pos = 255 - pos;
-  if (pos < 85) {
-    return rgb2Int(255 - pos * 3, 0, pos * 3);
-  } else if (pos < 170) {
-    pos -= 85;
-    return rgb2Int(0, pos * 3, 255 - pos * 3);
-  } else {
-    pos -= 170;
-    return rgb2Int(pos * 3, 255 - pos * 3, 0);
-  }
-};
-
-const rgb2Int = (r, g, b) => {
-  return ((r & 0xff) << 16) + ((g & 0xff) << 8) + (b & 0xff);
-};
-
 //pulse train 1
 io.sockets.on("connection", function(socket) {
   socket.on("direction", function(data) {
@@ -127,6 +110,7 @@ io.sockets.on("connection", function(socket) {
       let offset = 0;
       const channel = ws281x(100, { stripType: "ws2812" });
       const colorsArray = channel.array;
+
       const rainbowInterval = setInterval(() => {
         for (let i = 0; i < channel.count; i++) {
           colorsArray[i] = colorwheel((offset + i) % 256);
@@ -134,6 +118,23 @@ io.sockets.on("connection", function(socket) {
         offset = (offset + 1) % 256;
         ws281x.render(colorsArray);
       }, 1000 / 30);
+
+      const colorwheel = (pos) => {
+        pos = 255 - pos;
+        if (pos < 85) {
+          return rgb2Int(255 - pos * 3, 0, pos * 3);
+        } else if (pos < 170) {
+          pos -= 85;
+          return rgb2Int(0, pos * 3, 255 - pos * 3);
+        } else {
+          pos -= 170;
+          return rgb2Int(pos * 3, 255 - pos * 3, 0);
+        }
+      };
+
+      const rgb2Int = (r, g, b) => {
+        return ((r & 0xff) << 16) + ((g & 0xff) << 8) + (b & 0xff);
+      };
 
       console.log("LED COUNT", channel.count);
     }
