0c529f8d5b5fc2943de82e30c0322e52660e8a8b
Ryan Parr <parrfolio@gmail.com>
2022-02-26 08:26:37 -0800

    testing lights state

--- src/js/components/UserHome.js
+++ src/js/components/UserHome.js
@@ -45,7 +45,7 @@ export const UserHome = (props, state) => {
 
   useEffect(() => {
     console.log("Lights in View", isActive);
-    setActive(props.location.state.lightsActive);
+    // setActive(props.location.state.lightsActive);
     console.log("From Route Light Active", props.location.state.lightsActive);
 
     if (isActive) {
