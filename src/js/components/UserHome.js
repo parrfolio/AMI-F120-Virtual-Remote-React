f917dadb58649ec4dafe8e8bbd6666367b3cc6ba
Ryan Parr <parrfolio@gmail.com>
2022-02-26 09:45:57 -0800

    testing

--- src/js/components/UserHome.js
+++ src/js/components/UserHome.js
@@ -19,9 +19,7 @@ export const UserHome = (props, state) => {
   const [socket, setSocket] = useState(null);
   const [socketConnected, setSocketConnected] = useState(false);
 
-  const [isActive, setActive] = useState(
-    props.location.state != undefined && props.location.state != null
-  );
+  const [isActive, setActive] = useState(null);
 
   const toggleClass = () => {
     setActive(!isActive);
