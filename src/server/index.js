dcac099c326dc5c47ebc54e3fb3b8f6aad612fd2
Ryan Parr <parrfolio@gmail.com>
2022-03-09 19:21:16 -0800

    trying gpio 19

--- src/server/index.js
+++ src/server/index.js
@@ -143,7 +143,7 @@ io.sockets.on("connection", function(socket) {
         },
         {
           count: 120,
-          gpio: 12,
+          gpio: 19,
           invert: false,
           brightness: 255,
           stripType: "ws2812",
