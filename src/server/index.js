0c33f00fb8bcffe3de4e3890d2262b5194e8772c
Ryan Parr <parrfolio@gmail.com>
2022-03-12 10:52:01 -0800

    testing

--- src/server/index.js
+++ src/server/index.js
@@ -23,8 +23,7 @@ http.listen(PORT, () => {
 });
 
 //pulse train selections=
-// const pin = 32; //gpio 12
-const pin = 7; //gpio
+const pin = 32;
 
 // pulse speed settings that seem to be working with my stepper
 const pulseSpeed = 70;
@@ -39,42 +38,6 @@ const pulseDelay = 30;
 // const pulseDelay = 30;
 
 gpio.setup(pin, gpio.DIR_OUT);
-
-//channel 1 strips
-let rainbowInterval = null;
-let rainbowInterval2 = null;
-let rainbowInterval3 = null;
-let rainbowInterval4 = null;
-
-//channel 2 strips here
-let rainbowInterval5 = null;
-let rainbowInterval6 = null;
-
-let RecurringTimer = (callback, delay) => {
-  var timerId,
-    start,
-    remaining = delay;
-
-  var pause = function() {
-    console.log("pause was called");
-    clearTimeout(timerId);
-    remaining -= new Date() - start;
-  };
-
-  let resume = function() {
-    start = new Date();
-    timerId = setTimeout(function() {
-      remaining = delay;
-      resume();
-      callback();
-    }, remaining);
-  };
-
-  this.resume = resume;
-  this.pause = pause;
-
-  this.resume();
-};
 
 //PULSE TRAINS FOR STEPPER
 //pulse train 1
@@ -91,13 +54,6 @@ io.sockets.on("connection", function(socket) {
 
     if (data.state === "on") {
       ws281x.reset();
-      //channel 1 strips
-      rainbowInterval.pause();
-      rainbowInterval2.pause();
-      //channel 2 strips
-      rainbowInterval3.pause();
-      rainbowInterval4.pause();
-      ws281x.finalize();
       (async () => {
         console.log("=======-- Train 1 START --=======");
         for (let i = 0; i < data.ptrains[0]; i++) {
@@ -162,59 +118,69 @@ io.sockets.on("connection", function(socket) {
   };
 
   //twinkle
-  // let TwinkleColors = [
-  //   0xffffff,
-  //   0xfcfcfc,
-  //   0xfafafa,
-  //   0xf7f7f7,
-  //   0xf5f5f5,
-  //   0xf2f2f2,
-  //   0xf0f0f0,
-  //   0xededed,
-  //   0xebebeb,
-  //   0xe8e8e8,
-  //   0xe5e5e5,
-  //   0xe3e3e3,
-  //   0xe0e0e0,
-  //   0xdedede,
-  //   0xdbdbdb,
-  //   0xd9d9d9,
-  //   0xd6d6d6,
-  //   0xd4d4d4,
-  //   0xd1d1d1,
-  //   0xcfcfcf,
-  //   0xcccccc,
-  //   0xc9c9c9,
-  //   0xc7c7c7,
-  //   0xc4c4c4,
-  //   0xc2c2c2,
-  //   0xbfbfbf,
-  //   0xbdbdbd,
-  //   0xbababa,
-  //   0xb8b8b8,
-  //   0xb5b5b5,
-  //   0xb3b3b3,
-  //   0xb0b0b0,
-  // ];
+  let TwinkleColors = [
+    0xffffff,
+    0xfcfcfc,
+    0xfafafa,
+    0xf7f7f7,
+    0xf5f5f5,
+    0xf2f2f2,
+    0xf0f0f0,
+    0xededed,
+    0xebebeb,
+    0xe8e8e8,
+    0xe5e5e5,
+    0xe3e3e3,
+    0xe0e0e0,
+    0xdedede,
+    0xdbdbdb,
+    0xd9d9d9,
+    0xd6d6d6,
+    0xd4d4d4,
+    0xd1d1d1,
+    0xcfcfcf,
+    0xcccccc,
+    0xc9c9c9,
+    0xc7c7c7,
+    0xc4c4c4,
+    0xc2c2c2,
+    0xbfbfbf,
+    0xbdbdbd,
+    0xbababa,
+    0xb8b8b8,
+    0xb5b5b5,
+    0xb3b3b3,
+    0xb0b0b0,
+  ];
 
-  // let WasTwinkling = false;
-  // let TwinkleSpeed = 250;
-  // let LastStates = [];
+  let WasTwinkling = false;
+  let TwinkleSpeed = 250;
+  let LastStates = [];
 
-  // let getRandomInt = function(min, max) {
-  //   return Math.floor(Math.random() * (max - min + 1)) + min;
-  // };
+  let getRandomInt = function(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  };
 
-  // let GetNextColor = function(col, rand) {
-  //   var ind = TwinkleColors.indexOf(col);
-  //   if (ind == TwinkleColors.length + 1) {
-  //     // choose the first
-  //     return TwinkleColors[0];
-  //   } else {
-  //     // choose the next
-  //     return TwinkleColors[ind + 1];
-  //   }
-  // };
+  let GetNextColor = function(col, rand) {
+    var ind = TwinkleColors.indexOf(col);
+    if (ind == TwinkleColors.length + 1) {
+      // choose the first
+      return TwinkleColors[0];
+    } else {
+      // choose the next
+      return TwinkleColors[ind + 1];
+    }
+  };
+
+  //channel 1 strips
+  let rainbowInterval = null;
+  let rainbowInterval2 = null;
+  let rainbowInterval3 = null;
+  let rainbowInterval4 = null;
+
+  //channel 2 strips here
+  let rainbowInterval5 = null;
+  let rainbowInterval6 = null;
 
   socket.on("lights", function(data) {
     console.log("Lights", data.state);
@@ -264,9 +230,35 @@ io.sockets.on("connection", function(socket) {
       });
     });
 
+    function RecurringTimer(callback, delay) {
+      var timerId,
+        start,
+        remaining = delay;
+
+      var pause = function() {
+        console.log("pause was called");
+        clearTimeout(timerId);
+        remaining -= new Date() - start;
+      };
+
+      let resume = function() {
+        start = new Date();
+        timerId = setTimeout(function() {
+          remaining = delay;
+          resume();
+          callback();
+        }, remaining);
+      };
+
+      this.resume = resume;
+      this.pause = pause;
+
+      this.resume();
+    }
+
     if (data.state === "on") {
       //channel 1 stips
-      rainbowInterval = RecurringTimer(function() {
+      rainbowInterval = new RecurringTimer(function() {
         for (let i = 0; i < 150; i++) {
           colorsArray1[i] = colorwheel((offset + i) % 256);
         }
@@ -274,7 +266,7 @@ io.sockets.on("connection", function(socket) {
         ws281x.render();
       }, 1000 / 30);
 
-      rainbowInterval2 = RecurringTimer(function() {
+      rainbowInterval2 = new RecurringTimer(function() {
         for (let i = 150; i < 300; i++) {
           colorsArray1[i] = colorwheel((offset + i) % 256);
         }
@@ -318,7 +310,7 @@ io.sockets.on("connection", function(socket) {
       // }, 1000 / 30);
 
       //channel 2 strips
-      rainbowInterval3 = RecurringTimer(function() {
+      rainbowInterval3 = new RecurringTimer(function() {
         for (let i = 0; i < 20; i++) {
           colorsArray2[i] = colorwheel((offset + i) % 256);
         }
@@ -331,7 +323,7 @@ io.sockets.on("connection", function(socket) {
       }
       ws281x.render();
 
-      rainbowInterval4 = RecurringTimer(function() {
+      rainbowInterval4 = new RecurringTimer(function() {
         for (let i = 40; i < 60; i++) {
           colorsArray2[i] = colorwheel((offset + i) % 256);
         }
