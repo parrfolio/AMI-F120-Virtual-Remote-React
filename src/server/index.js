d43c8635ac2260f7e8f76ade5bb473056c12852c
Ryan Parr <parrfolio@gmail.com>
2022-03-13 13:35:26 -0700

    teset

--- src/server/index.js
+++ src/server/index.js
@@ -117,7 +117,7 @@ io.sockets.on("connection", function(socket) {
   let rainbowInterval5 = null;
   let rainbowInterval6 = null;
 
-  var RecurringTimer = class RecurringTimer {
+  class RecurringTimer {
     constructor(callback, delay) {
       var timerId,
         start,
@@ -143,6 +143,23 @@ io.sockets.on("connection", function(socket) {
 
       this.resume();
     }
+  }
+
+  let colorwheel = (pos) => {
+    pos = 255 - pos;
+    if (pos < 85) {
+      return this.rgb2Int(255 - pos * 3, 0, pos * 3);
+    } else if (pos < 170) {
+      pos -= 85;
+      return this.rgb2Int(0, pos * 3, 255 - pos * 3);
+    } else {
+      pos -= 170;
+      return this.rgb2Int(pos * 3, 255 - pos * 3, 0);
+    }
+  };
+
+  let rgb2Int = (r, g, b) => {
+    return ((r & 0xff) << 16) + ((g & 0xff) << 8) + (b & 0xff);
   };
 
   socket.on("lights", function(data) {
