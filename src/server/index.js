aa7e128ae45e6b3abbfab9018e59bf7baf22e543
Ryan Parr <parrfolio@gmail.com>
2022-03-13 12:24:59 -0700

    missing new on a function

--- src/server/index.js
+++ src/server/index.js
@@ -166,7 +166,7 @@ io.sockets.on("connection", function(socket) {
 
     if (data.state === "on") {
       //channel 1 stips
-      rainbowInterval = reocurringTimer(function() {
+      rainbowInterval = new reocurringTimer(function() {
         for (let i = 0; i < 150; i++) {
           colorsArray1[i] = common.colorwheel((offset + i) % 256);
         }
