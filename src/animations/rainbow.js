3c5eefca8fe1259b782e769a24281eccbcc00234
Ryan Parr <parrfolio@gmail.com>
2022-03-13 15:17:06 -0700

    test

--- src/animations/rainbow.js
+++ src/animations/rainbow.js
@@ -37,17 +37,18 @@ let colorsArray2 = channel2.array;
 let RecurringTimer = timers.RecurringTimer;
 
 function Rainbow(interval, delay, pause) {
-  if (!pause) {
-    interval = new RecurringTimer(function() {
+  interval = new RecurringTimer(function() {
+    if (!pause) {
       for (let i = 0; i < 150; i++) {
         colorsArray1[i] = common.colorwheel((offset + i) % 256);
       }
       offset = (offset + 1) % 256;
       ws281x.render();
-    }, delay);
-  } else {
-    interval.pause();
-  }
+    } else {
+      console.log("pause callled");
+      interval.pause();
+    }
+  }, delay);
 }
 
 module.exports = {
