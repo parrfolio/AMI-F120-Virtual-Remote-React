1ff14a5dbff733eb2919c70d2e2f078f53ae7d38
Ryan Parr <parrfolio@gmail.com>
2022-03-13 17:50:21 -0700

    test

--- src/animations/rainbow.js
+++ src/animations/rainbow.js
@@ -39,7 +39,7 @@ let RecurringTimer = timers.RecurringTimer;
 let interval = null;
 
 function Rainbow(config) {
-  console.log();
+  console.log(config);
   interval = new RecurringTimer(function() {
     for (let i = config[1].start; i < config[1].stop; i++) {
       colorsArray1[i] = common.colorwheel((offset + i) % 256);
