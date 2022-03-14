be24ba2634813094af9d6d9a6c76ea8b230c466e
Ryan Parr <parrfolio@gmail.com>
2022-03-14 16:15:11 -0700

    test

--- src/animations/rainbow.js
+++ src/animations/rainbow.js
@@ -10,9 +10,9 @@ function Rainbow(config) {
   console.log(Strip, Strip.colorsArray1, Strip());
   interval[config.name] = new RecurringTimer(function() {
     for (let i = config.start; i < config.stop; i++) {
-      Strip.colorsArray1[i] = common.colorwheel((strip.offset + i) % 256);
+      Strip.colorsArray1[i] = common.colorwheel((Strip.offset + i) % 256);
     }
-    offset = (strip.offset + 1) % 256;
+    offset = (Strip.offset + 1) % 256;
     ws281x.render();
   }, config.delay);
 
