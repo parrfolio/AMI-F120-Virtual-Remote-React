1dbd9284e5c481b2eaf2beb1dd88a4644571203f
Ryan Parr <parrfolio@gmail.com>
2022-03-14 20:52:53 -0700

    test

--- src/animations/rainbow.js
+++ src/animations/rainbow.js
@@ -10,10 +10,27 @@ let colors = {};
 function Rainbow(config) {
   let offset = 0;
 
+  let colorwheel = (pos) => {
+    pos = 255 - pos;
+    if (pos < 85) {
+      return rgb2Int(255 - pos * 3, 0, pos * 3);
+    } else if (pos < 170) {
+      pos -= 85;
+      return rgb2Int(0, pos * 3, 255 - pos * 3);
+    } else {
+      pos -= 170;
+      return rgb2Int(pos * 3, 255 - pos * 3, 0);
+    }
+  };
+
+  let rgb2Int = (r, g, b) => {
+    return ((r & 0xff) << 16) + ((g & 0xff) << 8) + (b & 0xff);
+  };
+
   colors[config.channel] = strip.FindStrip(config.channel);
   interval[config.name] = new RecurringTimer(function() {
     for (let i = config.start; i < config.stop; i++) {
-      colors[config.channel][i] = common.colorwheel((offset + i) % 256);
+      colors[config.channel][i] = colorwheel((offset + i) % 256);
     }
     offset = (offset + 1) % 256;
     ws281x.render();
