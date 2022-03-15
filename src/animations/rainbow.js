ca97f772838f03c9962d98a4123030c669236a4e
Ryan Parr <parrfolio@gmail.com>
2022-03-15 06:59:22 -0700

    test

--- src/animations/rainbow.js
+++ src/animations/rainbow.js
@@ -8,16 +8,24 @@ let interval = {};
 let colors = {};
 
 function Rainbow(config) {
+  const sleep = (milliseconds) => {
+    return new Promise((resolve) => setTimeout(resolve, milliseconds));
+  };
   let offset = 0;
 
   colors[config.channel] = strip.FindStrip(config.channel);
-  interval[config.name] = new RecurringTimer(function() {
-    for (let i = config.start; i < config.stop; i++) {
-      colors[config.channel][i] = 0xffcc22;
-    }
-    offset = (offset + 1) % 256;
-    ws281x.render();
-  }, config.delay);
+
+  (async () => {
+    await sleep(colors[config.channel]);
+    interval[config.name] = new RecurringTimer(function() {
+      for (let i = config.start; i < config.stop; i++) {
+        colors[config.channel][i] = common.colorwheel((offset + i) % 256);
+      }
+
+      offset = (offset + 1) % 256;
+      ws281x.render();
+    }, config.delay);
+  })();
 
   this.RainbowPause = function(config) {
     interval[config.name].pause();
