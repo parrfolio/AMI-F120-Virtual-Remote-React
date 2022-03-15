deb3cf11774d2ed57eb9686bcd52b5d2edff075c
Ryan Parr <parrfolio@gmail.com>
2022-03-15 13:14:26 -0700

    test

--- src/animations/rainbow.js
+++ src/animations/rainbow.js
@@ -12,12 +12,7 @@ let colors = {};
 
 function Rainbow(config) {
   let offset = 0;
-  let foo = new Strip(config, function() {
-    console.log("In Channel Set Name");
-    console.log(config);
-    console.log(this.findStrip(config.channelSet));
-    return this.findStrip(config);
-  });
+  let foo = new Strip().findStrip(config);
 
   console.log("OUTESIDE CHANNEL SET NAME");
   console.log(foo);
