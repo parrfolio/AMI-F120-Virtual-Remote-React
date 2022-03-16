db526c5f3b0756982e22c9fa252a65d21fe59901
Ryan Parr <parrfolio@gmail.com>
2022-03-15 21:42:16 -0700

    test

--- src/animations/rainbow.js
+++ src/animations/rainbow.js
@@ -6,28 +6,24 @@ const { Strip } = require("./strip");
 let RecurringTimer = timers.RecurringTimer;
 
 function Rainbow(config) {
-  let foo;
   config.forEach((item) => {
     let offset = 0;
-    foo = item.channelSetName;
     item.name = new Strip(item).findStrip();
-    foo = new RecurringTimer(function() {
+    item.channelSetName = new RecurringTimer(function() {
       for (let i = item.start; i < item.stop; i++) {
         item.name[i] = common.colorwheel((offset + i) % 256);
       }
       offset = (offset + 1) % 256;
       ws281x.render();
     }, item.delay);
+    this.RainbowPause = function(pause) {
+      pause.forEach((item) => {
+        ws281x.reset();
+        console.log(item);
+        item.channelSetName.pause();
+      });
+    };
   });
-
-  this.RainbowPause = function(pause) {
-    console.log(pause);
-    pause.forEach((item) => {
-      ws281x.reset();
-      console.log(item);
-      foo.pause();
-    });
-  };
 }
 module.exports = {
   Rainbow: Rainbow,
