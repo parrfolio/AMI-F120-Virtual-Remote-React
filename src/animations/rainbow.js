fb973a565fc9216fd315173a88aa8cc0ae51a54e
Ryan Parr <parrfolio@gmail.com>
2022-03-16 18:15:57 -0700

    test

--- src/animations/rainbow.js
+++ src/animations/rainbow.js
@@ -7,11 +7,11 @@ function Rainbow(config) {
   let strips = config;
   strips.forEach((item) => {
     let offset = 0;
-    item[stripArray] = new Strip(item).findStrip();
+    item["stripArray"] = new Strip(item).findStrip();
 
-    item[stripTimer] = new RecurringTimer(function() {
+    item["stripTimer"] = new RecurringTimer(function() {
       for (let i = item.start; i < item.stop; i++) {
-        item[stripArray][i] = common.colorwheel((offset + i) % 256);
+        item.stripArray[i] = common.colorwheel((offset + i) % 256);
       }
       offset = (offset + 1) % 256;
       ws281x.render();
@@ -20,7 +20,7 @@ function Rainbow(config) {
     this.RainbowPause = () => {
       strips.forEach((item) => {
         ws281x.reset(); //reset strips
-        item[stripTimer].pause();
+        item.stripTimer.pause();
       });
     };
   });
