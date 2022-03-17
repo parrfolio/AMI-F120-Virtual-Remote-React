a0c3afac351814349ba9e51fe923e63452b2d118
Ryan Parr <parrfolio@gmail.com>
2022-03-16 19:39:49 -0700

    test

--- src/animations/strip.js
+++ src/animations/strip.js
@@ -1,26 +1,27 @@
 const ws281x = require("@gbkwiatt/node-rpi-ws281x-native");
-const ledCount = 300;
-let strips = ws281x.init({
-  dma: 10,
-  freq: 800000,
-  channels: [
-    {
-      count: ledCount,
-      gpio: 18,
-      invert: false,
-      brightness: 255,
-      stripType: "ws2812",
-    },
-    {
-      count: ledCount,
-      gpio: 13,
-      invert: false,
-      brightness: 255,
-      stripType: "ws2812",
-    },
-  ],
-});
 function Strip(config) {
+  console.log(config);
+  const ledCount = 332;
+  let strips = ws281x.init({
+    dma: 10,
+    freq: 800000,
+    channels: [
+      {
+        count: ledCount,
+        gpio: 18,
+        invert: false,
+        brightness: config.brightness,
+        stripType: "ws2812",
+      },
+      {
+        count: ledCount,
+        gpio: 13,
+        invert: false,
+        brightness: config.brightness,
+        stripType: "ws2812",
+      },
+    ],
+  });
   let channelConfig = config.channelSet;
 
   const findStrip = () => {
