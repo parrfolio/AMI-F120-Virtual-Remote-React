6cff169fa2bca284f0cff8d4fd5d66ea9c1a6785
Ryan Parr <parrfolio@gmail.com>
2022-03-17 14:36:27 -0700

    test

--- src/animations/rainbow.js
+++ src/animations/rainbow.js
@@ -5,7 +5,7 @@ const { Strip } = require("./strip");
 
 function Rainbow(config) {
   let strips = config;
-  console.log(strips);
+  console.log("CONFIG!", strips, config);
   strips.forEach((item) => {
     let offset = 0;
     item["stripArray"] = new Strip(item).findStrip();
