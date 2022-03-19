1a0f226a59ce6a9e9f6e7c2c5121da37a8c8622a
Ryan Parr <parrfolio@gmail.com>
2022-03-19 07:24:42 -0700

    test

--- src/js/components/UserHome.js
+++ src/js/components/UserHome.js
@@ -64,7 +64,7 @@ export const UserHome = (props, state) => {
       return theme.name === animation;
     };
 
-    console.log("Theme Function", themeName);
+    console.log(themes.find(themeName));
 
     if (isActive) {
       socket.emit(
