cc939b4d15c5009f64b3eca4f34623a83969ca79
Ryan Parr <parrfolio@gmail.com>
2022-03-20 08:13:22 -0700

    test

--- src/js/components/ToggleButton.js
+++ src/js/components/ToggleButton.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 export const ToggleButton = (props) => {
   const { isActive, setActive, setAnimation, key, setAnimationName } = props;
-
+  console.log(key);
   return (
     <button
       onClick={() => {
