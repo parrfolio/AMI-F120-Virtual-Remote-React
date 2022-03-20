2ae08a97e57e953e935bd36bd912be1987ad2196
Ryan Parr <parrfolio@gmail.com>
2022-03-20 09:50:36 -0700

    test

--- src/js/components/ToggleButton.js
+++ src/js/components/ToggleButton.js
@@ -1,11 +1,15 @@
 import React, { useState, useCallback } from "react";
 export const ToggleButton = (props) => {
   const { isActive, setActive, setAnimation, key, AnimationName } = props;
+
   const handleActive = (key) => {
+    console.log("AnimationName", AnimationName);
+    console.log("KEY", key);
     let name = [...isActive];
+    console.log("NAME", name);
     name[key] = key;
 
-    console.log("NAME", (name[key] = key));
+    console.log("NAME MATCH", (name[key] = key));
 
     setActive(name);
   };
