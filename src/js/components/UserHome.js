ba78afab33731954e43643bf017f6cb40f50fd98
Ryan Parr <parrfolio@gmail.com>
2022-03-31 12:33:19 -0700

    testing

--- src/js/components/UserHome.js
+++ src/js/components/UserHome.js
@@ -64,28 +64,31 @@ export const UserHome = (props, state) => {
   useEffect(() => {
     console.log(appState.activeObject, appState.previousObject);
 
-    if (appState.activeObject != null) return;
-    if (appState.activeObject === appState.previousObject) {
-      socket.emit(
-        "lights",
-        {
-          state: "on",
-          animation: animation,
-          stripConf: themes[animation],
-        },
-        (data) => {}
-      );
-    } else {
-      socket.emit(
-        "lights",
-        {
-          state: "off",
-          animation: animation,
-          stripConf: themes[animation],
-        },
-        (data) => {}
-      );
-    }
+    console.log(appState.activeObject != null);
+    console.log(typeof appState.activeObject != null);
+
+    // if (appState.activeObject != null) return;
+    // if (appState.activeObject === appState.previousObject) {
+    //   socket.emit(
+    //     "lights",
+    //     {
+    //       state: "on",
+    //       animation: animation,
+    //       stripConf: themes[animation],
+    //     },
+    //     (data) => {}
+    //   );
+    // } else {
+    //   socket.emit(
+    //     "lights",
+    //     {
+    //       state: "off",
+    //       animation: animation,
+    //       stripConf: themes[animation],
+    //     },
+    //     (data) => {}
+    //   );
+    // }
   }, [appState]);
 
   const toggleActiveStyle = (index) => {
