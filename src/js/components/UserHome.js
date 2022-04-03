42f4fd96dfccb1b900eea4b8b657c1536b328330
Ryan Parr <parrfolio@gmail.com>
2022-04-03 08:46:14 -0700

    test

--- src/js/components/UserHome.js
+++ src/js/components/UserHome.js
@@ -42,17 +42,32 @@ export const UserHome = (props, state) => {
     if (appState.activeObject != null) {
       if (appState.objects[isActiveIndex] === appState.activeObject) {
         console.log("RUNNING?", isRunning);
-        socket.emit(
-          "lights",
-          {
-            state: "on",
-            animation: animation,
-            stripConf: themes[animation],
-          },
-          (response) => {
-            setRunning(response.running);
-          }
-        );
+        if (isRunning) {
+          socket.emit(
+            "lights",
+            {
+              state: "on",
+              animation: animation,
+              stripConf: themes[animation],
+            },
+            (response) => {
+              setRunning(response.running);
+            }
+          );
+        } else {
+          console.log("Running False from OFF Statement");
+          socket.emit(
+            "lights",
+            {
+              state: "off",
+              animation: animation,
+              stripConf: themes[animation],
+            },
+            (response) => {
+              setRunning(response.running);
+            }
+          );
+        }
       } else {
         socket.emit(
           "lights",
