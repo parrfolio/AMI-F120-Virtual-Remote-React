c7bb40e7f5ee2675c628fa1de4abde479681ecaf
Ryan Parr <parrfolio@gmail.com>
2022-04-03 10:05:08 -0700

    test

--- src/js/components/ToggleButton.js
+++ src/js/components/ToggleButton.js
@@ -24,7 +24,7 @@ export const ToggleButton = (props) => {
         setRunning(!isRunning);
       }}
     >
-      Name: {setAnimationName} {isRunning ? "active" : "inactive"}
+      Name: {setAnimationName} {running ? "active" : "inactive"}
     </button>
   );
 };
