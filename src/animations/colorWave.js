0d18666d3ab1887eed7eaa67c7ba4de8003f25da
Ryan Parr <parrfolio@gmail.com>
2022-04-14 07:10:34 -0700

    test

--- src/animations/colorWave.js
+++ src/animations/colorWave.js
@@ -14,22 +14,25 @@ function ColorWave(config) {
     item["stripArray"] = new Strip(item).findStrip();
     item["stripTimer"] = new RecurringTimer(function() {
       //item.stripArray[i]
-      if (iterationIndex < maxIterations) {
-        if (ledIndex < Strip.ledCount) {
-          item.stripArray[i] = common.colorwheel(
-            ((ledIndex * 256) / Strip.ledCount + iterationIndex) & 255
-          );
 
-          ledIndex++;
+      console.log(Strip.ledCount);
+      for (let i = item.start; i < item.stop; i++) {
+        if (iterationIndex < maxIterations) {
+          if (ledIndex < Strip.ledCount) {
+            item.stripArray[i] = common.colorwheel(
+              ((ledIndex * 256) / Strip.ledCount + iterationIndex) & 255
+            );
+
+            ledIndex++;
+          } else {
+            ledIndex = 0;
+            iterationIndex++;
+          }
         } else {
           ledIndex = 0;
-          iterationIndex++;
+          iterationIndex = 0;
         }
-      } else {
-        ledIndex = 0;
-        iterationIndex = 0;
       }
-
       ws281x.render();
     }, item.delay);
 
