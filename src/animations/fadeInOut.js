15b752b486581be86ab455bef8bafe92beb40237
Ryan Parr <parrfolio@gmail.com>
2022-04-19 07:42:06 -0700

    test

--- src/animations/fadeInOut.js
+++ src/animations/fadeInOut.js
@@ -27,12 +27,26 @@ function FadeInOut(config) {
 
     item["stripArray"] = new Strip(item).findStrip();
     item["stripTimer"] = new RecurringTimer(function() {
-      for (let i = item.start; i < item.stop; i++) {
-        for (let x = 0; x < eyeSize; x++) {
+      for (i = item.start; i < item.stop; i++) {
+        item.stripArray[i] = 0xf0b3b3;
+        for (x = 0; x < eyeSize; x++) {
           //   LastStates[x] = theEye[theEye.length - 1];
           console.log(x);
-          item.stripArray[x] = 0xcc0000;
+          item.stripArray[i + x] = 0xcc0000;
         }
+
+        item.stripArray[i + eyeSize + 1] = 0xf0b3b3;
+
+        // for(int i = 0; i < NUM_LEDS-EyeSize-2; i++) {
+        //     setAll(0,0,0);
+        //     setPixel(i, red/10, green/10, blue/10);
+        //     for(int j = 1; j <= EyeSize; j++) {
+        //       setPixel(i+j, red, green, blue);
+        //     }
+        //     setPixel(i+EyeSize+1, red/10, green/10, blue/10);
+        //     showStrip();
+        //     delay(SpeedDelay);
+        //   }
       }
 
       // some other animation
