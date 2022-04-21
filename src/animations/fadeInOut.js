24ae082aaf7a260b5874bb514116ab5de1e7c527
Ryan Parr <parrfolio@gmail.com>
2022-04-21 07:36:52 -0700

    test

--- src/animations/fadeInOut.js
+++ src/animations/fadeInOut.js
@@ -9,27 +9,13 @@ function FadeInOut(config) {
   // console.log(config);
   strips.forEach((item) => {
     let offset = 0;
-    let theEye = [
-      0xcc0000,
-      0xcc0000,
-      0xcc0000,
-      0xcc0000,
-      0xcc0000,
-      0xcc0000,
-      0xcc0000,
-      0xcc0000,
-      0xcc0000,
-      0xcc0000,
-    ];
     let leds = common.num_leds();
     let eyeSize = 25;
-    let LastStates = [];
-
     item["stripArray"] = new Strip(item).findStrip();
     item["stripTimer"] = new RecurringTimer(function() {
       for (i = item.start; i < item.stop; i++) {
         for (let x = 0; x < leds - eyeSize - 2; x++) {
-          item.stripArray[x] = 0xcc0000 / 10;
+          //item.stripArray[x] = 0xcc0000 / 10;
           //   setPixel(x, red / 10, green / 10, blue / 10);
           for (let j = 1; j <= eyeSize; j++) {
             item.stripArray[x + j] = 0xcc0000;
