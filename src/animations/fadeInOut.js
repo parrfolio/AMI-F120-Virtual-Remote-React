a307d874fa09815a2798dcc027da399c317dbc7f
Ryan Parr <parrfolio@gmail.com>
2022-04-24 11:23:50 -0700

    test

--- src/animations/fadeInOut.js
+++ src/animations/fadeInOut.js
@@ -16,13 +16,22 @@ function FadeInOut(config) {
     item["stripArray"] = new Strip(item).findStrip();
     item["stripTimer"] = new RecurringTimer(function() {
       if (item.name === "title_striplight_2") {
-        for (i = item.start; i < item.stop; i++) {
+        // for (i = item.start; i < item.stop; i++) {
+        //   item.stripArray[i] = common.cylon(
+        //     (offset + i) % leds,
+        //     0xcc0000,
+        //     leds
+        //   );
+        // }
+
+        for (i = item.stop; i >= item.start; i--) {
           item.stripArray[i] = common.cylon(
             (offset + i) % leds,
             0xcc0000,
             leds
           );
         }
+
         offset = (offset + 1) % leds;
         ws281x.render();
       }
