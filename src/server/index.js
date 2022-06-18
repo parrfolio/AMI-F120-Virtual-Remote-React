a180fc25cb26ef7e2dc5a97664019d3672c1a301
Ryan Parr <parrfolio@gmail.com>
2022-06-18 11:31:04 -0700

    test

--- src/server/index.js
+++ src/server/index.js
@@ -135,6 +135,10 @@ io.sockets.on("connection", function (socket) {
     (async () => {
       await sleep(15);
       rawTimedWrite(0x30, LCD_REGISTER_SELECT_CMND);
+      await sleep(4100);
+      rawTimedWrite(0x30, LCD_REGISTER_SELECT_CMND);
+      await sleep(100);
+      rawTimedWrite(0x30, LCD_REGISTER_SELECT_CMND);
     })();
   };
 
