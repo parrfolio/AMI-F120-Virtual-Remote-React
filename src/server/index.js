31951749ba4a535997dcbd382a829a10f622b391
Ryan Parr <parrfolio@gmail.com>
2022-06-18 11:59:21 -0700

    test

--- src/server/index.js
+++ src/server/index.js
@@ -127,7 +127,7 @@ io.sockets.on("connection", function (socket) {
     );
 
     (async () => {
-      await sleep(20);
+      await sleep(2);
     })();
   };
 
@@ -135,22 +135,19 @@ io.sockets.on("connection", function (socket) {
     (async () => {
       await sleep(15);
       rawTimedWrite(0x30, LCD_REGISTER_SELECT_CMND);
-      await sleep(4100);
+      await sleep(4.1);
       rawTimedWrite(0x30, LCD_REGISTER_SELECT_CMND);
-
-      //works above sort of
-
-      // await sleep(100);
-      // rawTimedWrite(0x30, LCD_REGISTER_SELECT_CMND);
-      // rawTimedWrite(0x20, LCD_REGISTER_SELECT_CMND);
-      // rawTimedWrite(0x20, LCD_REGISTER_SELECT_CMND);
-      // rawTimedWrite(0x80, LCD_REGISTER_SELECT_CMND);
-      // rawTimedWrite(0x00, LCD_REGISTER_SELECT_CMND);
-      // rawTimedWrite(0xc0, LCD_REGISTER_SELECT_CMND);
-      // rawTimedWrite(0x00, LCD_REGISTER_SELECT_CMND);
-      // rawTimedWrite(0x10, LCD_REGISTER_SELECT_CMND);
-      // rawTimedWrite(0x00, LCD_REGISTER_SELECT_CMND);
-      // rawTimedWrite(0x60, LCD_REGISTER_SELECT_CMND);
+      await sleep(100);
+      rawTimedWrite(0x30, LCD_REGISTER_SELECT_CMND);
+      rawTimedWrite(0x20, LCD_REGISTER_SELECT_CMND);
+      rawTimedWrite(0x20, LCD_REGISTER_SELECT_CMND);
+      rawTimedWrite(0x80, LCD_REGISTER_SELECT_CMND);
+      rawTimedWrite(0x00, LCD_REGISTER_SELECT_CMND);
+      rawTimedWrite(0xc0, LCD_REGISTER_SELECT_CMND);
+      rawTimedWrite(0x00, LCD_REGISTER_SELECT_CMND);
+      rawTimedWrite(0x10, LCD_REGISTER_SELECT_CMND);
+      rawTimedWrite(0x00, LCD_REGISTER_SELECT_CMND);
+      rawTimedWrite(0x60, LCD_REGISTER_SELECT_CMND);
     })();
   };
 
