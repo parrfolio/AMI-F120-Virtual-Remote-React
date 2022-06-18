6fd26bc6b54a04fdbc7d9d3fa007c8ae5acb9739
Ryan Parr <parrfolio@gmail.com>
2022-06-18 11:45:47 -0700

    test

--- src/server/index.js
+++ src/server/index.js
@@ -134,6 +134,7 @@ io.sockets.on("connection", function (socket) {
   const initializeLCD = () => {
     (async () => {
       await sleep(15);
+      rawTimedWrite(0x60, LCD_REGISTER_SELECT_CMND);
       rawTimedWrite(0x30, LCD_REGISTER_SELECT_CMND);
       await sleep(4100);
       rawTimedWrite(0x30, LCD_REGISTER_SELECT_CMND);
