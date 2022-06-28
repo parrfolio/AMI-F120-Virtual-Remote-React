3b7e39983a9aa73d8218d322941f6513a1812f65
Ryan Parr <parrfolio@gmail.com>
2022-06-28 13:04:50 -0700

    test

--- src/server/index.js
+++ src/server/index.js
@@ -165,14 +165,14 @@ io.sockets.on("connection", function (socket) {
             Buffer.from([cleanData | LCD_BACKLIGHT | cleanRS]),
             handleI2CError
           );
-          sleepMore.msleep(100);
+          sleepMore.msleep(1);
           IC2_bus.i2cWrite(
             LCD_IC2_ADDRESS,
             1,
             Buffer.from([cleanData | LCD_BACKLIGHT | LCD_ENABLE | cleanRS]),
             handleI2CError
           );
-          sleepMore.msleep(100);
+          sleepMore.msleep(1);
           IC2_bus.i2cWrite(
             LCD_IC2_ADDRESS,
             1,
@@ -187,13 +187,13 @@ io.sockets.on("connection", function (socket) {
         };
 
         const initializeLCD = () => {
-          sleepMore.msleep(200);
+          sleepMore.msleep(2);
           rawTimedWrite(0x30, LCD_REGISTER_SELECT_CMND);
-          sleepMore.msleep(100);
+          sleepMore.msleep(1);
           rawTimedWrite(0x30, LCD_REGISTER_SELECT_CMND);
-          sleepMore.msleep(100);
+          sleepMore.msleep(1);
           rawTimedWrite(0x30, LCD_REGISTER_SELECT_CMND);
-          sleepMore.msleep(10);
+          sleepMore.msleep(1);
           rawTimedWrite(0x30, LCD_REGISTER_SELECT_CMND);
           rawTimedWrite(0x20, LCD_REGISTER_SELECT_CMND);
           rawTimedWrite(0x20, LCD_REGISTER_SELECT_CMND);
