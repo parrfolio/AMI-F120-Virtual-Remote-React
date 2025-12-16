179b81cbd46aafd47c09ae3d487bb59ac27814d6
Ryan Parr <ryanparr@MacBookPro.lan>
2025-12-15 17:36:55 -0800

    Make classic static white; default brightness 192

--- src/animations/classic.cjs
+++ src/animations/classic.cjs
@@ -1,59 +1,31 @@
 const ws281x = require("@gbkwiatt/node-rpi-ws281x-native");
-const common = require("./common.cjs");
-const { RecurringTimer } = require("./timer.cjs");
 const { Strip } = require("./strip.cjs");
 
 let activeStrips = [];
 
 function Classic(config) {
   activeStrips = config;
-  // console.log(config);
+
+  // Classic is static: set colors once and render once.
   activeStrips.forEach((item) => {
-    let offset = 0;
-    item["stripArray"] = new Strip(item).findStrip();
-    item["stripTimer"] = new RecurringTimer(function () {
-      let color = 0x000000; // default black
-      switch (item.name) {
-        case "record_rack":
-          color = 0xffffff;
-          break;
-        case "cabinet_accent":
-          color = 0xffffff;
-          break;
-        case "titlestrips_bottom":
-          color = 0xffffff;
-          break;
-        case "titlestrips_top":
-          color = 0xffffff;
-          break;
-        case "extra_leds":
-          color = 0x000000; // off
-          break;
-        case "cabinet_ami_logo":
-          color = 0xffffff;
-          break;
-        case "door_light":
-          color = 0xffffff;
-          break;
-      }
-      for (let i = item.start; i < item.stop; i++) {
-        item.stripArray[i] = color;
-      }
-      ws281x.render();
-    }, item.delay);
+    item.stripArray = new Strip(item).findStrip();
+
+    // All strips white, except extra_leds off
+    const color = item.name === "extra_leds" ? 0x000000 : 0xffffff;
+
+    for (let i = item.start; i < item.stop; i++) {
+      item.stripArray[i] = color;
+    }
   });
+
+  ws281x.render();
 }
 
 function ClassicPause() {
   ws281x.reset();
-  activeStrips.forEach((item) => {
-    if (item.stripTimer) {
-      item.stripTimer.pause();
-    }
-  });
 }
 
 module.exports = {
-  Classic: Classic,
-  ClassicPause: ClassicPause,
+  Classic,
+  ClassicPause,
 };
