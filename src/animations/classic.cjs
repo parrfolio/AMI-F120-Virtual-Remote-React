06c4659401125436874ea9ca74ba517886c85dc5
Ryan Parr <ryanparr@MacBookPro.lan>
2025-12-15 17:07:28 -0800

    still trying to fix classic to be all white

--- src/animations/classic.cjs
+++ src/animations/classic.cjs
@@ -1,56 +1,59 @@
 const ws281x = require("@gbkwiatt/node-rpi-ws281x-native");
+const common = require("./common.cjs");
+const { RecurringTimer } = require("./timer.cjs");
 const { Strip } = require("./strip.cjs");
 
 let activeStrips = [];
 
 function Classic(config) {
   activeStrips = config;
-
-  // Set all colors once - no need for timers since colors are static
+  // console.log(config);
   activeStrips.forEach((item) => {
-    item.stripArray = new Strip(item).findStrip();
-
-    let color = 0x000000;
-
-    switch (item.name) {
-      case "record_rack":
-        color = 0xffffff; // light yellow
-        break;
-      case "cabinet_accent":
-        color = 0xffffff; // light yellow
-        break;
-      case "titlestrips_bottom":
-        color = 0xffffff; // bright white
-        break;
-      case "titlestrips_top":
-        color = 0xffffff; // bright white
-        break;
-      case "extra_leds":
-        color = 0x000000; // off
-        break;
-      case "cabinet_ami_logo":
-        color = 0xffffff; // bright white
-        break;
-      case "door_light":
-        color = 0xffffff; // red
-        break;
-    }
-
-    for (let i = item.start; i < item.stop; i++) {
-      item.stripArray[i] = color;
-    }
+    let offset = 0;
+    item["stripArray"] = new Strip(item).findStrip();
+    item["stripTimer"] = new RecurringTimer(function () {
+      let color = 0x000000; // default black
+      switch (item.name) {
+        case "record_rack":
+          color = 0xffffff; // white
+          break;
+        case "cabinet_accent":
+          color = 0xffffff; // green
+          break;
+        case "titlestrips_bottom":
+          color = 0xffffff; // yellow
+          break;
+        case "titlestrips_top":
+          color = 0xffffff; // dodger blue
+          break;
+        case "extra_leds":
+          color = 0xffffff; // off
+          break;
+        case "cabinet_ami_logo":
+          color = 0xffffff; // red
+          break;
+        case "door_light":
+          color = 0xffffff; // red
+          break;
+      }
+      for (let i = item.start; i < item.stop; i++) {
+        item.stripArray[i] = color;
+      }
+      ws281x.render();
+    }, item.delay);
   });
-
-  // Render once after all strips are set
-  ws281x.render();
 }
 
 function ClassicPause() {
   ws281x.reset();
-  // No timers to pause since Classic is static
+  activeStrips.forEach((item) => {
+    if (item.stripTimer) {
+      item.stripTimer.pause();
+    }
+  });
 }
 
 module.exports = {
-  Classic,
-  ClassicPause,
+  Classic: Classic,
+  ClassicPause: ClassicPause,
 };
